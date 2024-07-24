b0c8d5d387f8761514b4105c4005638635f42790
ThePhaseless <kukubaorch@gmail.com>
2024-07-24 20:41:15 +0000

    fix logger

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,7 @@
 import logging
+import os
 
 logger = logging.getLogger("uvicorn.error")
+logger.setLevel(os.getenv("LOG_LEVEL") or logging.INFO)
+if len(logger.handlers) == 0:
+    logger.addHandler(logging.StreamHandler())
