77fd2a47f7226a1adbff5f9d0829812ecc904387
ThePhaseless <kukubaorch@gmail.com>
2024-11-24 22:00:58 +0000

    remaster

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-./run_vnc.sh
-
-# Activate virtual environment
-. .venv/bin/activate && python3 main.py
--- src/utils/consts.py
+++ src/utils/consts.py
@@ -1,31 +1,5 @@
 import logging
 import os
-from pathlib import Path
 
 LOG_LEVEL = os.getenv("LOG_LEVEL") or "INFO"
 LOG_LEVEL = logging.getLevelNamesMapping()[LOG_LEVEL.upper()]
-
-UBLOCK_TITLE = "uBO Lite — Dashboard"
-
-CHALLENGE_TITLES = [
-    # Cloudflare
-    "Just a moment...",
-    # DDoS-GUARD
-    "DDoS-Guard",
-]
-
-GITHUB_WEBSITES = [
-    "https://github.com/",
-    "https://www.github.com/",
-    "github.com",
-    "www.github.com",
-]
-
-EXTENSION_REPOSITIORIES = [
-    "OhMyGuus/I-Still-Dont-Care-About-Cookies",
-    "uBlockOrigin/uBOL-home",
-]
-
-SLEEP_SECONDS = 1
-
-EXTENSIONS_PATH = Path(".extentions")
