a14a5ca32f8a5c0edb1a454f2b5026bff4f914cd
ThePhaseless <kukubaorch@gmail.com>
2025-08-17 06:57:20 +0200

    create ADDON PATH

--- src/consts.py
+++ src/consts.py
@@ -1,13 +1,19 @@
 import logging
 import os
+from pathlib import Path
 
-from playwright_captcha import CaptchaType  # pyright: ignore[reportMissingTypeStubs]
+from playwright_captcha import CaptchaType
+from playwright_captcha.utils.camoufox_add_init_script.add_init_script import (
+    get_addon_path,
+)
 
 LOG_LEVEL = logging.getLevelNamesMapping()[os.getenv("LOG_LEVEL", "INFO").upper()]
 
 VERSION = os.getenv("VERSION", "unknown").removeprefix("v")
 
-HEADLESS_MODE = os.getenv("HEADLESS_MODE", "virtual")
+HEADLESS_MODE = os.getenv("HEADLESS_MODE") or True
+
+ADDON_PATH = str(Path(get_addon_path()).absolute())
 
 PROXY = os.getenv("PROXY")
 
