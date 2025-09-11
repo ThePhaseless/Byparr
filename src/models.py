8ceb7fa7d33bf537218bcb00ff14b74031ee03a8
ThePhaseless <kukubaorch@gmail.com>
2025-09-11 16:55:05 +0000

    change healthcheck msg

--- src/models.py
+++ src/models.py
@@ -25,7 +25,7 @@ class LinkRequest(BaseModel):
 
 class HealthcheckResponse(BaseModel):
     model_config = {"alias_generator": to_camel, "populate_by_name": True}
-    msg: str = "Byparr is ready!"
+    msg: str = "Byparr is working!"
     version: str = consts.VERSION
     user_agent: str
 
